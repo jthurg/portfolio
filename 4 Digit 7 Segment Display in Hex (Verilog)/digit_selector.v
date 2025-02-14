773fea97645806f3f87e2efd550d20c0a804b78c
jthurg <156131276+jthurg@users.noreply.github.com>
2025-02-13 22:04:10 -0700

    Update digit_selector.v

--- 4 Digit 7 Segment Display in Hex (Verilog)/digit_selector.v
+++ 4 Digit 7 Segment Display in Hex (Verilog)/digit_selector.v
@@ -1,22 +1,9 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
+////////////////////////////////////////////////////////////////////////////////// 
 // Create Date: 02/11/2025 02:08:17 PM
-// Design Name: 
 // Module Name: digit_selector
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+// Project Name: 4 Digit 7 Segment Display in Hex
+// Target Devices: Basys 3 Xilinx FPGA Board
 //////////////////////////////////////////////////////////////////////////////////
 
 //Define module for digit_selector and signals.
