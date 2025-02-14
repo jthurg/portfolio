5d03b915b0253c5e36784fd82cfd7de2e80bf5a9
jthurg <156131276+jthurg@users.noreply.github.com>
2025-02-13 22:02:47 -0700

    Update seg7display.v

--- 4 Digit 7 Segment Display in Hex (Verilog)/seg7display.v
+++ 4 Digit 7 Segment Display in Hex (Verilog)/seg7display.v
@@ -1,22 +1,9 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
 // Create Date: 01/23/2025 01:37:23 PM
-// Design Name: 
 // Module Name: seg7display
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+// Project Name: 4 Digit 7 Segment Display in Hex
+// Target Devices: Basys 3 Xilinx FPGA Board
 //////////////////////////////////////////////////////////////////////////////////
 
 //Define the seg7display module and its signals.
